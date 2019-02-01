6086b4f0c93562dc988738849923a3f73f9d0bfe
Zoltan Sisak <zoltan.sisak@gmail.com>
2019-02-01 16:48:23 +0100

    json.load

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def hook():
 		return webhookMessage
 	if request.method == 'POST':
 		webhookMessage = request.json
-		print(webhookMessage)
+		print(json.loads(webhookMessage)["data"])
 		return jsonify(webhookMessage)
 
 
